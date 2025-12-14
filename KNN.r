d05b8d50200b21da02bd88ac1e75bf1679e4c6ca
Giulia Di Vincenzo <giuliadivi02@gmail.com>
2025-12-14 14:44:12 +0100

    KNN.r

--- KNN.r
+++ KNN.r
@@ -2,10 +2,19 @@ library(MASS)
 library(ISLR)
 library(vcd)
 library(caret)
-str(df_outliers)
 ##Partición de Data usando library(caret)
 
 # KPI
+mape<- function(truth, pred){
+  truth <- as.numeric(truth)
+  pred  <- as.numeric(pred)
+  idx0 <- which(truth == 0)
+  out <- abs(truth - pred) / abs(truth)
+  if(length(idx0) > 0){
+    out[idx0] <- (abs(truth[idx0] - pred[idx0]) + 1) / (abs(truth[idx0]) + 1)
+  }
+  mean(out, na.rm = TRUE)
+}
 KPI <- function(data, lev = NULL, model = NULL) {
   eps <- 1e-6  
   mape <- mean(abs((data$obs - data$pred) / (data$obs + eps))) * 100
@@ -19,12 +28,12 @@ KPI <- function(data, lev = NULL, model = NULL) {
 
 #------
 set.seed(1)
-trainingg <- subset(df_outliers, dataset == "train")
+trainingg <- subset(df_all_transformed, dataset == "train")
 inTraining <- createDataPartition(trainingg$song_popularity, p = .80, list = FALSE)
 training <- trainingg[inTraining,]
 testing  <- trainingg[-inTraining,]
-training <- training[, -c(1:3, ncol(training))]
-testing <- testing[, -c(1:3, ncol(testing))]
+training <- training[, -c(1:3)]
+testing <- testing[, -c(1:3)]
 set.seed(1)
 
 model3 <- train(
@@ -32,7 +41,7 @@ model3 <- train(
   data = training,
   method = 'knn',
   preProcess = c("center", "scale"),
-  tuneGrid = data.frame(k = c(3, 5, 7))
+  tuneGrid = data.frame(k = c(10, 12, 14))
 )
 model3
 ###
@@ -45,8 +54,7 @@ sqrt(mean((test.target - predictions)^2))
 # R2
 cor(test.target, predictions) ^ 2
 # MAPE
-eps <- 1e-6 
-mean(abs((test.target - predictions) / (test.target + eps))) * 100
+mape(test.target, predictions)
 ### Si agregamos cross-validation
 ctrl <- trainControl(
   method = "cv",
@@ -58,7 +66,8 @@ model4 <- train(
   data = training,
   method = 'knn',
   preProcess = c("center", "scale"),
-  trControl = ctrl
+  trControl = ctrl,
+  tuneGrid = data.frame(k = c(10, 12, 14))
 )
 model4
 test.features = subset(testing, select=-c(song_popularity))
@@ -70,14 +79,13 @@ sqrt(mean((test.target - predictions)^2))
 # R2
 cor(test.target, predictions) ^ 2
 # MAPE
-eps <- 1e-6 
-mean(abs((test.target - predictions) / (test.target + eps))) * 100
+mape(test.target, predictions)
 
 ##Tunning the model (GridSearch for k)
 set.seed(1)
 
 tuneGrid <- expand.grid(
-  k = seq(5, 9, by = 1)
+  k = seq(9, 20, by = 1)
 )
 
 model5 <- train(
@@ -90,11 +98,14 @@ model5 <- train(
 )
 model5
 plot(model5)
+test.features = subset(testing, select=-c(song_popularity))
+test.target = subset(testing, select=song_popularity)[,1]
+predictions = predict(model5, newdata = test.features)
+
 # RMSE
 sqrt(mean((test.target - predictions)^2))
 # R2
 cor(test.target, predictions) ^ 2
 # MAPE
-eps <- 1e-6 
-mean(abs((test.target - predictions) / (test.target + eps))) * 100
+mape(test.target, predictions)
 
